413925d0b6613202557322433bcd71da779b393f
christianlincke <christian.lincke@haw-hamburg.de>
2024-06-19 13:21:15 +0200

    Data Analyzer and minor changes

---
+++ DataAnalyzer.py
@@ -0,0 +1,77 @@
+import pandas as pd
+import glob
+import os
+import numpy as np
+from prettytable import PrettyTable
+import matplotlib.pyplot as plt
+
+# Create writer for logging
+#writer = SummaryWriter(log_dir=f'runs/arms_3D_{ARM}_{time.asctime()}')
+
+# Path to the directory containing the CSV files
+path = f'TrainData/'
+dir_list = os.listdir(path)
+
+print('Available directories: \n')
+
+idx = 1
+for dir in dir_list:
+    print(f'[{idx}] {dir}')
+    idx += 1
+
+dir = int(input('\nWhich do you want to analyze?'))
+print('->', dir_list[dir-1] + '\n')
+
+# get all the available files
+csv_files = glob.glob(path + dir_list[dir-1] + '/*.csv')
+
+if len(list(csv_files)) == 0:
+    print('Its empty!')
+    quit()
+
+print('These files are available:\n')
+
+for file in list(csv_files):
+    print(file)
+
+print('\nAnalyze data...\n')
+# Read and concatenate all CSV files into a single DataFrame
+data_frames = [pd.read_csv(file) for file in csv_files]
+data = pd.concat(data_frames, ignore_index=True)
+data_array = data.to_numpy()
+
+labels = np.array([])
+for row in data_array:
+    labels = np.append(labels, [row[0]])
+
+table = PrettyTable()
+bins, counts = np.unique(labels, return_counts=True)
+
+table.field_names = bins
+table.add_row(counts)
+print(table)
+
+count_min, count_max = min(counts), max(counts)
+
+plt.bar(bins, counts)
+plt.ylim(count_min - (count_min / 100), count_max + (count_min / 100))
+plt.show()
+
+#print(f'Length: {}')
+
+#counts, bins = np.histogram(data)
+
+#print(counts, bins)
+
+# Create DataLoader for PyTorch
+
+
+
+"""
+# log a histogram of all targets/predictions
+writer.add_histogram('Targets/Test', epoch_targets_test, epoch)
+writer.add_histogram('Predictions/Test', epoch_pred_test, epoch)
+
+writer.flush()
+writer.close()
+"""
