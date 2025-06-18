18d476a4239db0ee28bbb51752cadf1d65548acc
christianlincke <christian.lincke@haw-hamburg.de>
2025-06-18 14:31:07 +0200

    Added PyQT for interactive plotting

--- Helpers/plotLandmarksPose.py
+++ Helpers/plotLandmarksPose.py
@@ -5,16 +5,18 @@ Useful toi check if mirroring works as intended
 Last changes by Christian 19. Jul 24
 """
 import csv
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 from Models import ArmModel
 from mpl_toolkits.mplot3d import Axes3D
+mpl.use('Qt5Agg')
 
 # What parameter?
-PARAM = "stretch" # or stretch
+PARAM = "dir" # or stretch
 
 # Plotting mode - 3d or 2d
-DIMENSIONS = '2d'
+DIMENSIONS = '3d'
 
 # Random Sample?
 RND = True
@@ -28,8 +30,8 @@ else:
 SAMPLE_IDX = 5
 
 # Filenames for left and right samples
-path_left = '../TrainData/test_left/stretch_data_left_2024_Jul_22_131549.csv'
-path_right = '../TrainData/test_right/stretch_data_right_2024_Jul_22_131549.csv'
+path_left = '../TrainData/dir_data_left/dir_data_left_2024_Jul_23_115005.csv'
+path_right = '../TrainData/dir_data_right/dir_data_right_2024_Jul_23_115005.csv'
 
 # Get landmark masks
 mask_left = ArmModel.landmarkMask('left')
